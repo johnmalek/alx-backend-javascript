fd1f449316f14e1df61b591cab35655b099789ec
malek <johnmalek54@gmail.com>
2023-03-20 20:04:54 +0000

    Corrections

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,5 +1,4 @@
 export default function taskBlock(trueOrFalse) {
-
   if (trueOrFalse) {
     var task = true;
     var task2 = false;
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
 export default function returnHowManyArguments(...theArgs) {
-    let total = 0;
-    for (const arg of theArgs) {
-      total ++;
-    }
-    return total;
+  return theArgs.length;
 }
