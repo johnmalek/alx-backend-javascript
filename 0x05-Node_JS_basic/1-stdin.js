098dc38ba211f65904b4dfd67cd8a7e9805381ee
Malek <johnmalek54@gmail.com>
2023-05-16 20:17:01 +0000

    Task 1 corrections

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,12 @@
+/* eslint-disable */
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('readable', function() {
-  var chunk = process.stdin.read();
+process.stdin.on('readable', function () {
+  const chunk = process.stdin.read();
   if (chunk !== null) {
     process.stdout.write('Your name is: ' + chunk);
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', function () {
   process.stdout.write('This important software is now closing');
 });
