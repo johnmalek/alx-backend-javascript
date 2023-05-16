f00d8ac3a1bc6499bae2ae782eb9334db14b771e
Malek <johnmalek54@gmail.com>
2023-05-16 21:15:46 +0000

    Task 6 - create a small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-app.get('get', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!')
 });
 
